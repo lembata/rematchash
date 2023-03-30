866adba914c17b89e90f662de1a24ff887ca4fc4
Alexander Lemberg <alexander.lemberg@siriussoftware.bg>
2023-03-30 13:47:47 +0300

    Added duplication check

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use clap::Parser;
+use md5;
 use std::fs;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
-use clap::Parser;
-use md5;
 
 ///Simple tool to delete files based on a hash
 #[derive(Parser)]
@@ -14,10 +14,10 @@ struct Cli {
 
     ///The hash to search for
     #[arg(short = 'H', long)]
-    hash: Vec<String>,
+    hash: String,
 
     ///Ignore symlinks
-    #[arg(short = 's', long, default_value = "true")]
+    #[arg(short = 's', long, default_value = "false")]
     ignore_symlinks: bool,
 
     ///Scan subdirectories
@@ -27,58 +27,119 @@ struct Cli {
     ///Ask for confirmation before deleting for each file
     #[arg(short, long, default_value = "false")]
     interactive: bool,
-}
 
+    ///Output more information
+    #[arg(short, long, default_value = "false")]
+    verbose: bool,
+}
 
 fn main() {
     let args = Cli::parse();
 
-    if !Path::new(&args.path).exists() {
+    if !Path::new(&args.path).exists() || !Path::new(&args.path).is_dir(){
         println!("Directory does not exist: {}", args.path.display());
         return;
     }
 
-    let affected_files = scan_directory(&args.path,&args);
+    if !is_valid_hash(&args.hash) {
+        println!("Invalid hash: {}", &args.hash);
+        return;
+    }
 
+    let mut handled_paths: Vec<PathBuf> = Vec::new();
+    let affected_files = scan_directory(&args.path, &args, &mut handled_paths);
     println!("Deleted {} file(s)", affected_files);
 }
 
-fn scan_directory(path: &PathBuf, cli: &Cli) -> i32 {
+fn scan_directory(path: &PathBuf, cli: &Cli, handled_paths: &mut Vec<PathBuf>) -> i32 {
     let mut count = 0;
+
+    if cli.verbose {
+        println!("Scanning directory: {}", path.display());
+    }
+
+    handled_paths.push(path.clone());
+
     std::fs::read_dir(path).unwrap().for_each(|entry| {
         let entry = entry.unwrap();
+        let entry_path = entry.path();
 
-        if cli.ignore_symlinks && Path::new(&entry.path()).is_symlink() {
-            return;
-        }
-        else if Path::new(&entry.path()).is_dir() {
-            if cli.recursive {
-                count += scan_directory(&entry.path(), cli);
+        if entry_path.is_symlink() {
+            if cli.ignore_symlinks {
+                if cli.verbose {
+                    println!("Ignoring symlink: {}", entry_path.display());
+                }
+                return;
             }
         }
 
-        if Path::new(&entry.path()).is_file() {
+        let entry_path = if entry_path.is_symlink() {
+            if cli.verbose {
+                println!("Resolving symlink: {} -> {}", entry_path.display(), entry_path.read_link().unwrap().display());
+            }
+            fs::read_link(entry_path).unwrap()
+        } else {
+            entry_path
+        };
+
+        if handled_paths.contains(&entry_path) {
+            if cli.verbose {
+                println!("Ignoring duplicate: {}", entry_path.display());
+            }
+            return;
+        }
+
+        handled_paths.push(entry_path.clone());
+
+        if entry_path.is_dir() {
+
+            if cli.recursive {
+                count += scan_directory(&entry.path(), cli, handled_paths);
+            }
+        } else {
             let hash = md5::compute(fs::read(&entry.path()).unwrap());
             let hash = format!("{:x}", hash);
-            if cli.hash.contains(&hash) {
 
+            if cli.verbose {
+                println!("{}: {}", entry.path().display(), hash);
+            }
+
+            if cli.hash.contains(&hash) {
                 if cli.interactive {
-                    print!("Delete file {} [y/N]: ", entry.path().display());
-                    std::io::stdout().flush().expect("flush failed!");
-                        //.flush().expect("flush failed!");
-                    let mut input = String::new();
-                    std::io::stdin().read_line(&mut input).unwrap();
-                    if input.trim() != "y" {
+                    if !confirmation_promt(&entry.path()) {
                         return;
                     }
                 }
 
                 if let Err(e) = fs::remove_file(&entry.path()) {
                     println!("Failed to delete file: {}", e);
+                    return;
+                } else {
+                    if cli.verbose {
+                        println!("Deleted file: {}", entry.path().display());
+                    }
                 }
+
                 count += 1;
             }
         }
     });
+
     count
 }
+
+fn confirmation_promt(path: &PathBuf) -> bool {
+    print!("Delete file {} [y/N]: ", path.display());
+    std::io::stdout().flush().expect("flush failed!");
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    input.trim().to_lowercase() == "y"
+}
+
+fn is_valid_hash(hash: &String) -> bool {
+    hash.len() == 32
+        && hash.chars().all(|c| c.is_digit(16))
+}
+
